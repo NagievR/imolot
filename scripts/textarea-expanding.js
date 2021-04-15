c19c7249e76a2d1d14971c4a5ab07e417bc7c84c
Ruslan Nagiev <ultraduck100@gmail.com>
2021-04-15 12:23:21 +0300

    desktop chat is done

---
+++ scripts/textarea-expanding.js
@@ -0,0 +1,13 @@
+const textarea = document.querySelector('.chat__textarea');
+const bottomBorderWidth = parseInt(getComputedStyle(textarea).borderBottomWidth);
+const topBorderWidth = parseInt(getComputedStyle(textarea).borderTopWidth);
+const bordersHorizontal = bottomBorderWidth + topBorderWidth;
+
+const resize = () => {
+  const scrollHeight = textarea.scrollHeight;
+  textarea.style.height = scrollHeight + bordersHorizontal + 'px';
+};
+
+textarea.addEventListener('input', resize);
+
+resize();
